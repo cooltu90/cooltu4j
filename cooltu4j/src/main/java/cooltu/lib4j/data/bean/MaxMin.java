465475da7201096dfd7fa0019e729fa50c257f06
cooltu <cooltu@codingtu.com>
2023-04-20 08:43:46 +0800

    v1.0.8修改MaxMin

--- cooltu4j/src/main/java/cooltu/lib4j/data/bean/MaxMin.java
+++ cooltu4j/src/main/java/cooltu/lib4j/data/bean/MaxMin.java
@@ -5,20 +5,24 @@ import cooltu.lib4j.tools.MathTool;
 
 import java.util.List;
 
-public class MaxMin<T extends Number> {
+public class MaxMin<NUM extends Number, T> extends CoreBean {
 
-    public T max;
-    public T min;
+    public NUM max;
+    public NUM min;
 
-    public static <T extends Number> MaxMin<T> obtian(List<T> list) {
-        MaxMin<T> maxMin = new MaxMin<T>();
+    public static interface Getter<NUM extends Number, T> {
+        NUM getNum(T t);
+    }
+
+    public static <NUM extends Number, T> MaxMin<NUM, T> obtian(List<T> list, Getter<NUM, T> getter) {
+        MaxMin<NUM, T> maxMin = new MaxMin<NUM, T>();
         int count = CountTool.count(list);
         if (count > 0) {
-            T num = list.get(0);
+            NUM num = getter.getNum(list.get(0));
             maxMin.max = num;
             maxMin.min = num;
             for (int i = 1; i < count; i++) {
-                num = list.get(i);
+                num = getter.getNum(list.get(i));
                 maxMin.max = MathTool.max(maxMin.max, num);
                 maxMin.min = MathTool.min(maxMin.min, num);
             }
